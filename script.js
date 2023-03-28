72974aeb3037745b6ced2769ad3e8c38c5f1125d
Amey  Pankanti <86099941+AmeyPank@users.noreply.github.com>
2023-03-28 22:50:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+var nameElement = document.querySelector("#name");
+var ageElement = document.querySelector("#age");
+var formElement = document.querySelector("form");
+function handleSubmit(){
+	alert ("abc");
+	e.preventDefault()
+}
+
+formElement.addEventListner('submit', handelSubmit);
