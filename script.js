1398fc36f6cd55d67a25dbb85f3b527a2059ffea
AmeyPank <pankantiameya@.com>
2023-04-12 00:59:14 +0530

    submission created

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //your JS code here. If required.
-//your JS code here. If required.
 var nameElement = document.querySelector("#name");
 var ageElement = document.querySelector("#age");
 var btnElement = document.querySelector("#btn");
