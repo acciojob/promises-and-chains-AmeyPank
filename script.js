5a3c1cdc32212b10a329b693a9b43a1109300e89
Amey  Pankanti <86099941+AmeyPank@users.noreply.github.com>
2023-04-11 13:39:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 //your JS code here. If required.
+//your JS code here. If required.
 var nameElement = document.querySelector("#name");
 var ageElement = document.querySelector("#age");
-var formElement = document.querySelector("form");
-function handleSubmit(){
-	alert ("abc");
-	e.preventDefault()
+var btnElement = document.querySelector("#btn");
+let calc=()=>{
+var promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    var age = ageElement.value;
+    var name = nameElement.value;
+   // var age = ageElement.value;
+   //  var name = nameElement.value;
+   if (!(age && name)) {
+     alert("Please enter valid details");
+    return;
+   }
+    if (age < 18) {
+      reject(`Oh sorry ${name}. You aren't old enough.`);
+    } else {
+      resolve(`Welcome, ${name}. You can vote.`);
+    }
+  }, 4000);
+});
+return promise;
 }
 
-formElement.addEventListner('submit', handelSubmit);
+// function callPromise() {
+//     return promise;
+// }
+function handleSubmit(e) {
+    e.preventDefault();
+  calc().then((data)=>{
+	  alert(data);
+  }).catch((data)=>{
+	  alert(data);
+  });
+}
+
+btnElement.addEventListener("click", handleSubmit);
